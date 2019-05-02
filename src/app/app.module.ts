4149972712ada908ff929f6b000911a2de35e42a
Austin Roa <ausroa@gmail.com>
2019-05-01 20:12:53 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SlideshowModule} from 'ng-simple-slideshow';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomepageComponent } from './views/homepage/homepage.component';
+import { FooterComponent } from './components/footer/footer.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlideshowModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
